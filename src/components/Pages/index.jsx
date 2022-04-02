47af26ea538696878cf04ab3650164ec8742bca0
Cheng Yufeng <chengyufeng@zhihu.com>
2022-04-02 11:04:10 +0800

    feat: 解决初始化无数据问题

--- src/components/Pages/index.jsx
+++ src/components/Pages/index.jsx
@@ -1,10 +1,14 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import './index.css'
 const Pages = (props) => {
     const total = props.total;
+    const init = [];
+    for (let i = 1; i <= total && init.length < 5; i++) {
+        init.push(i);
+    }
     const [cur, setCur] = useState(1);
     const [value, setValue] = useState('')
-    const [items, setItems] = useState([]);
+    const [items, setItems] = useState(init);
     const [begin, setBegin] = useState(0);
     const [len, setLen] = useState(0);
 
@@ -25,11 +29,7 @@ const Pages = (props) => {
         let temp = []
         for (let i = 0; i < len; i++) {
             let showItem = begin + i;
-            if (cur === showItem){
-                temp.push({num : showItem, isCurrent : true});
-            } else {
-                temp.push({num : showItem, isCurrent : false});
-            }
+            temp.push(showItem);
             setItems(temp);
         }
     }, [cur])
@@ -44,35 +44,35 @@ const Pages = (props) => {
     }
 
     const goNext = () => {
-        if(cur < total){
+        if (cur < total) {
             setCur(cur + 1);
         }
     }
 
     const goPrev = () => {
-        if(cur > 1){
+        if (cur > 1) {
             setCur(cur - 1);
         }
     }
 
     const goPage = () => {
-        if(!/^[1-9]\d*$/.test(value)){
+        if (!/^[1-9]\d*$/.test(value)) {
             alert('页码只能输入大于0的正整数');
-        }else if(value > total){
+        } else if (value > total) {
             alert('没有这么多页');
-        }else{
+        } else {
             setCur(value);
         }
     }
-   
+
     return (
         <div className="pagnation-ui">
-            <a className={cur === 1 ? 'prev disable' : 'prev'}  onClick={() => goPrev()}></a>
+            <a className={cur === 1 ? 'prev disable' : 'prev'} onClick={() => goPrev()}></a>
             <span className="pagnation-columns">
                 {
                     items.map(item => {
                         return (
-                            <a key={item.num} onClick={() => handleClick(item.num)} className={item.isCurrent ? 'num current' : 'num'}>{item.num}</a>
+                            <a key={item} onClick={() => handleClick(item)} className={item === cur ? 'num current' : 'num'}>{item}</a>
                         )
                     })
                 }
